4f2601ac08d8a1d812e305bc10c144a8105f6f8f
Richard McGain <mcgain@gmail.com>
2011-10-02 19:37:27 +1100

    ooh, dry

--- lib/monster.rb
+++ lib/monster.rb
@@ -1,45 +1,50 @@
 require "happymapper"
 
-class AbilityScoreNumber
-	include HappyMapper
-	tag :AbilityScoreNumber
-	element :Name, String
-	attribute :FinalValue, Integer
+	def standard_name_value
+		include HappyMapper
+		element :Name, String, :deep => true
+		attribute :FinalValue, Integer, :deep => true
+	end
 
-end
+	class StandardNameValue
+		def to_s
+			"#{self.Name}: #{self.FinalValue}\n"
+		end
+	end
 
-class AbilityScoreValues
-	include HappyMapper
-	tag :Values
-	has_many :AbilityScoreNumber, AbilityScoreNumber, :deep => true
-end
+	class AbilityScores < StandardNameValue		
+		standard_name_value
+		tag :AbilityScoreNumber
+	end
 
+	class Defenses < StandardNameValue	
+		standard_name_value
+		tag :SimpleAdjustableNumber
+	end
+ 
+	class AttackBonuses < StandardNameValue	
+		standard_name_value
+		tag :CalculatedNumber
+	end
+		
+	class Skills < StandardNameValue	
+		standard_name_value
+		tag :SkillNumber
+	end
 
-class AbilityScores
-	include HappyMapper
-	tag :AbilityScores
-	has_one :Values, AbilityScoreValues
-end
-
-class Defenses
-	include HappyMapper
-	tag :SimpleAdjustableNumber
-	element :Name, String
-	attribute :FinalValue, Integer, :deep => true
-end
-
-class Monster
-	include HappyMapper
-	tag :Monster
-	element :Level, Integer
-	element :Experience, Integer
-	element :CompendiumUrl, String
-	element :Name, String
-	element :IsLeader, Boolean
-	has_one :AbilityScores, AbilityScores
-	has_many :Defenses, Defenses, :deep => true
-
-end
+	class Monster
+		include HappyMapper
+		tag :Monster
+		element :Level, Integer
+		element :Experience, Integer
+		element :CompendiumUrl, String
+		element :Name, String
+		element :IsLeader, Boolean
+		has_many :AbilityScores, AbilityScores, :deep => true
+		has_many :Defenses, Defenses, :deep => true
+		has_many :AttackBonuses, AttackBonuses,	:deep => true
+		has_many :Skills, Skills, :deep => true
+	end
 
 if File.exists?("Goblin.xml")
 	monster = Monster.parse(File.read("Goblin.xml"))
@@ -50,10 +55,15 @@ end
 
 
 print monster.Name + "\n"
-
-monster.AbilityScores.Values.AbilityScoreNumber.each do |score|
-	print score.Name + ":" + score.FinalValue.to_s + "\n"
+print "Ability Scores\n".upcase!
+monster.AbilityScores.each do |score|
+	print score
 end
+print "Defenses\n".upcase!
 monster.Defenses.each do |defense| 
-	print defense.Name + ":" + defense.FinalValue.to_s + "\n"
+	print defense.Name + ": " + defense.FinalValue.to_s + "\n"
+end
+print "AttackBonuses\n".upcase!
+monster.AttackBonuses.each do |bonus|
+	print bonus.Name + ": " + bonus.FinalValue.to_s + "\n"
 end
